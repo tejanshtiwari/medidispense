5ee6f8c1d87b144b95a895c0150b44d8667c0b64
TEJANSH TIWARI <tejanshtiwari2410@gmail.com>
2026-09-26 04:41:48 +0000

    fix layout and apply clean gitignore

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { Analytics } from '@vercel/analytics/next'
 import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import './global.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
